c5e9b5284b3a9930f2577e37dbed0414798c409a
Phummyphum <110968491+Phummyphum@users.noreply.github.com>
2023-02-24 16:17:05 +0700

    Update lab21.h

--- lab21.h
+++ lab21.h
@@ -62,8 +62,48 @@ void Unit::showStatus(){
 void Unit::newTurn(){
 	guard_on = false;
 }
+bool Unit::isDead(){
+  if(hp <= 0) return true;
+  else return false;
+}
+void Unit::guard(){
+  guard_on = true;
+}
+int Unit::beAttacked(int oppatk){
+  int dmg = 0;
+    if(guard_on == false){
+        dmg = oppatk-def;
+      hp = hp-dmg;
+       
+    }
+    if(guard_on == true){
+      dmg = (oppatk-def)/3;
+      hp = hp - dmg;
+    
+    }
+    if(hp < 0){
+        hp = 0;
+    }
+    return dmg;
+}
 
-
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
+int Unit::heal(){
+  int h;
+  int temp;
+  h = rand()%21 +10;
+  if(hp+h >= hpmax){
+    temp = hpmax-hp;
+    hp = hpmax;
+    return temp;
+  }else{ 
+      hp = hp+h;
+      return h;
+    }
+  
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
 //Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
